560bd2cb3a7281f96d7043c727ebd4e29c6edb67
huangzhe <huangzhe@jbufa.com>
2025-11-13 09:17:59 +0800

    tiny

--- utils/svm2hdf5.py
+++ utils/svm2hdf5.py
@@ -12,10 +12,10 @@ from utils import set_logging, make_dirs, load_data
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    # parser.add_argument('--path_label_train', default='/home/Huangzhe/test/afd_pm_train')
-    # parser.add_argument('--save_dir', default='/dev/shm/afd_pm_hdf5')
-    parser.add_argument('--path_label_train', default='/home/manu/tmp/afd_pm_train')
-    parser.add_argument('--save_dir', default='/home/manu/tmp/afd_pm_hdf5')
+    parser.add_argument('--path_label_train', default='/home/Huangzhe/test/afd_pm_train')
+    parser.add_argument('--save_dir', default='/dev/shm/afd_pm_hdf5')
+    # parser.add_argument('--path_label_train', default='/home/manu/tmp/afd_pm_train')
+    # parser.add_argument('--save_dir', default='/home/manu/tmp/afd_pm_hdf5')
     return parser.parse_args()
 
 
