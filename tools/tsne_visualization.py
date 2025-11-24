fffb5938a609a8c15a8dfbee7a57ee4366be8d99
huangzhe <huangzhe@jbufa.com>
2025-11-24 08:30:38 +0800

    tiny

--- tools/tsne_visualization.py
+++ tools/tsne_visualization.py
@@ -99,7 +99,7 @@ def parse_args():
     parser.add_argument(
         '--error_threshold',
         type=float,
-        default=0.01,
+        default=0.003125,
         help="重构误差阈值，用于高亮显示'难重构'的正常样本。")
     parser.add_argument(
         '--show_plot',
